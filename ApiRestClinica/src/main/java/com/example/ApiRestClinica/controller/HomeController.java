ac8ee26f0b85456ec6b9af2ba2730bc25fbea6eb
salosmc <salo_smc@hotmail.com>
2022-05-22 22:35:03 -0300

    add confiiguraciones heroku

---
+++ ApiRestClinica/src/main/java/com/example/ApiRestClinica/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.ApiRestClinica.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping()
+    String home() {
+        return "Welcome to my Dental Clinic API Rest!";
+    }
+}
+
